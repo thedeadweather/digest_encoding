57b54b0ff822c0b2eed0b0f6b9f1db25039294a8
Alex Kln <kalininmalinin@gmail.com>
2020-04-16 23:41:27 +0300

    add sha2 method

--- encrypt.rb
+++ encrypt.rb
@@ -1,4 +1,3 @@
-
 # Подключаем встроенную в ruby библиотеку с модулем Digest
 require "digest"
 
@@ -10,10 +9,11 @@ string = STDIN.gets.chomp
 puts "Каким способом зашифровать:"
 puts "1. MD5"
 puts "2. SHA1"
+puts "3. SHA2"
 encrypt_method = STDIN.gets.to_i
 
-until encrypt_method.between?(1, 2)
-  puts "Выберите 1 или 2"
+until encrypt_method.between?(1, 3)
+  puts "Выберите 1 2 или 3"
   encrypt_method = STDIN.gets.to_i
 end
 
@@ -21,6 +21,7 @@ puts "Вот что получилось:"
 
 # Выводим зашифрованную строку в зависимости от выбора пользователя
 case encrypt_method
-when 1 then puts Digest::MD5.hexdigest(string)
-when 2 then puts Digest::SHA1.hexdigest(string)
+  when 1 then puts Digest::MD5.hexdigest(string)
+  when 2 then puts Digest::SHA1.hexdigest(string)
+  when 3 then puts Digest::SHA2.hexdigest(string)
 end
